c72739caca5723b9f1e31941bd97977612075702
Patrick Grady <patickrgrady81@gmail.com>
2020-06-17 13:27:37 -0400

    project review 1

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,7 +7,6 @@ class API::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
-
     if user 
       session[:user_id] = user.id
       render json: {logged_in: true, user: user}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 User.destroy_all
-User.create!(email:"pat@pat.com", password:"pat", password_confirmation:"pat")
+User.create!(email:"pat@pat.com", password:"password", password_confirmation:"password", city:"Philadelphia, PA")
 
 puts "Created #{ActionController::Base.helpers.pluralize(User.all.count, 'User')}..."
