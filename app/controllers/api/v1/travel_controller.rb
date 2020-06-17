1a26ee957e0068d11d9fabf209bc909c032a66b5
Patrick Grady <patickrgrady81@gmail.com>
2020-06-17 09:04:19 -0400

    Working on sort

--- app/controllers/api/v1/travel_controller.rb
+++ app/controllers/api/v1/travel_controller.rb
@@ -75,6 +75,36 @@ class API::V1::TravelController < ApplicationController
         end
       end
     end
+    #   this is what I would sort on
+    #   events[0]["_embedded"]["events"][0]["dates"]["start"]["localDate"]
+    #
+    #   And this is what I would get
+    #   "localDate"=>"2020-09-03",
+    #   "localTime"=>"19:30:00"
+    #   "dateTime"=>"2020-09-03T23:30:00Z"
+    #
+    #   events[0]["_embedded"]["events"][0]["dates"]["start"]["dateTime"]
+    #   gives me ISO 8601
+    #   .to_time gives me 2020-09-03 23:30:00 +0000 a Time object
+    #   time.month => 9
+    #   time.day => 3
+    #   time.year => 2020
+    #   time.hour => 23
+    #   time.strftime("%m/%d/%Y") => "09/03/2020"
+    #   time.strftime("%I:%M %p") => "11:30 PM"
+    #   time.strftime("%A") => "Thursday"
+    #
+    #
+    #   Gives list of dateTimes
+    #   events.map.with_index do |e, i|
+    #     e["_embedded"]["events"][0]["dates"]["start"]["dateTime"] 
+    #   end  
+    #
+    #   events.sort_by do |e|
+    #     e["_embedded"]["events"][0]["dates"]["start"]["dateTime"] 
+    #   end  
+
+    # binding.pry
     render json: events
   end
 end
